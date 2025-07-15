a5ab51e37a28f5eff9ffcc445335531ef96684fc
oscarherra <oscar.herra@ucr.ac.cr>
2025-07-15 12:16:15 -0600

    Fix: Corregir configuración de transpileDependencies

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,23 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  transpileDependencies: [
+    '@vercel/analytics'
+  ],
+
+  pwa: {
+    name: 'Horario-AEG',
+    themeColor: '#ffffff',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'img/icons/aeglogo.png',
+      favicon16: 'img/icons/aeglogo.png',
+      appleTouchIcon: 'img/icons/aeglogo.png',
+      maskIcon: 'img/icons/aeglogo.png',
+      msTileImage: 'img/icons/aeglogo.png'
+    }
+  }
+};
